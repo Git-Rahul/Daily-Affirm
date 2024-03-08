4d4c981c68566271df5400b0b936570b28cbd5ee
Git-Rahul <kumarr977@gmail.com>
2024-03-09 03:29:55 +0530

    Added Daily Affirmation files via local upload

---
+++ App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import bgimg from "./assets/darkgreen.jpg";
+
+function App() {
+  const [affirmation, setAffirmation] = useState([]);
+
+  const card = {
+    height: "65vh",
+    width: "350px",
+    background: "linear-gradient(150deg, #1f1f1f , #4f4f4f)",
+    margin: "auto",
+    borderRadius: "15px",
+    boxShadow: "6px 6px 12px  #1b1b1b",
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api")
+      .then((response) => response.json())
+      .then((data) => setAffirmation(data))
+      .catch((error) => console.log("Error fetching affirmation!", error));
+  }, []);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // const handlePrev = () => {
+  //   setCurrentIndex((prevIndex) => (prevIndex + 1) % affirmation.length);
+  // };
+
+  const handleNext = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + affirmation.length) % affirmation.length
+    );
+  };
+
+  return (
+    <>
+      {/* <div
+        style={{
+          margin: "0px",
+          padding: "0px",
+          backgroundImage: `url(${bgimg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "200vh",
+          width: "100vw"
+        }}
+      > */}
+        <h1>Daily Affirmations</h1>
+        <div style={card}>
+          <h2 style={{ width: "300px", margin: "auto", paddingTop: "40%" }}>
+            "{affirmation[currentIndex]}."
+            {/* {affirmation.map((affirm) => (
+            <div key={affirm}>{affirm}</div>
+          ))} */}
+          </h2>
+        </div>
+        <br />
+        {/* <button onClick={handlePrev}>Previous</button> */}
+        <button onClick={handleNext}>SURPRISE ME</button>
+      {/* </div> */}
+    </>
+  );
+}
+
+export default App;
